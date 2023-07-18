c49cd73b06f204cc115149c65015fd37615aa390
Gleb Gurgurov <gurragyrra@gmail.com>
2023-07-18 20:33:24 +0300

    end 3rd section nodejs course

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,12 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 const notes = getNotes()
 
 console.log(notes)
 console.log(validator.isEmail('foo@bar.com'))
+
+console.log(chalk.inverse.bold.green('Success'))
+console.log(chalk.inverse.underline.yellow('Warning'))
+console.log(chalk.dim.inverse.strikethrough.red('Error'))
